5ce7206b73767bcc62b1e8eb0eba03606c519d17
root <root@omco.su>
2022-11-23 02:09:57 +0300

    formatting

--- test/inc.test.ts
+++ test/inc.test.ts
@@ -5,34 +5,34 @@ import { splitBytes } from "./common.ts";
 const testInc = await Deno.readFile(".build/inc.na.bin");
 
 Deno.test({
-	// testing varying mov instructions, 32 bit
-	name: "test jasm - inc/dec",
-	fn: async (t) => {
-		// create factory to emit linux binaries in 32 bit mode
-		const { inc, dec } = factory({
-			arch: "x86",
-			withFormat: "elf",
-		});
+  // testing varying mov instructions, 32 bit
+  name: "test jasm - inc/dec",
+  fn: async (t) => {
+    // create factory to emit linux binaries in 32 bit mode
+    const { inc, dec } = factory({
+      arch: "x86",
+      withFormat: "elf",
+    });
 
-		// set up byte splitter to process consequent instructions
-		const bytes = splitBytes(testInc, 1);
+    // set up byte splitter to process consequent instructions
+    const bytes = splitBytes(testInc, 1);
 
-		await t.step({
-			name: "inc eax",
-			fn: () => {
-				// real bytes from nasm run
-				const realValue = [...bytes.next().value];
-				// assert that jasm emitted the same thing
-				assertEquals(inc("eax"), realValue);
-			},
-		});
+    await t.step({
+      name: "inc eax",
+      fn: () => {
+        // real bytes from nasm run
+        const realValue = [...bytes.next().value];
+        // assert that jasm emitted the same thing
+        assertEquals(inc("eax"), realValue);
+      },
+    });
 
-		await t.step({
-			name: "dec ebx",
-			fn: () => {
-				const realValue = [...bytes.next().value];
-				assertEquals(dec("ebx"), realValue);
-			},
-		});
-	},
+    await t.step({
+      name: "dec ebx",
+      fn: () => {
+        const realValue = [...bytes.next().value];
+        assertEquals(dec("ebx"), realValue);
+      },
+    });
+  },
 });
--- test/mov.test.ts
+++ test/mov.test.ts
@@ -5,83 +5,83 @@ import { splitBytes } from "./common.ts";
 const testMovRR = await Deno.readFile(".build/mov.rr.bin");
 
 Deno.test({
-	// testing varying mov instructions, 32 bit
-	name: "test jasm - mov reg reg",
-	fn: async (t) => {
-		// create factory to emit linux binaries in 32 bit mode
-		const { mov } = factory({
-			arch: "x86",
-			withFormat: "elf",
-		});
+  // testing varying mov instructions, 32 bit
+  name: "test jasm - mov reg reg",
+  fn: async (t) => {
+    // create factory to emit linux binaries in 32 bit mode
+    const { mov } = factory({
+      arch: "x86",
+      withFormat: "elf",
+    });
 
-		// set up byte splitter to process consequent instructions
-		const bytes = splitBytes(testMovRR);
+    // set up byte splitter to process consequent instructions
+    const bytes = splitBytes(testMovRR);
 
-		// run first test as defined in mov.asm
-		await t.step({
-			name: "mov eax, eax",
-			fn: () => {
-				// real bytes from nasm run
-				const realValue = [...bytes.next().value];
-				// assert that jasm emitted the same thing
-				assertEquals(mov("eax", "eax"), realValue);
-			},
-		});
+    // run first test as defined in mov.asm
+    await t.step({
+      name: "mov eax, eax",
+      fn: () => {
+        // real bytes from nasm run
+        const realValue = [...bytes.next().value];
+        // assert that jasm emitted the same thing
+        assertEquals(mov("eax", "eax"), realValue);
+      },
+    });
 
-		await t.step({
-			name: "mov eax, ebx",
-			fn: () => {
-				const realValue = [...bytes.next().value];
-				assertEquals(mov("eax", "ebx"), realValue);
-			},
-		});
+    await t.step({
+      name: "mov eax, ebx",
+      fn: () => {
+        const realValue = [...bytes.next().value];
+        assertEquals(mov("eax", "ebx"), realValue);
+      },
+    });
 
-		await t.step({
-			name: "mov ecx, edx",
-			fn: () => {
-				const realValue = [...bytes.next().value];
-				assertEquals(mov("ecx", "edx"), realValue);
-			},
-		});
-	},
+    await t.step({
+      name: "mov ecx, edx",
+      fn: () => {
+        const realValue = [...bytes.next().value];
+        assertEquals(mov("ecx", "edx"), realValue);
+      },
+    });
+  },
 });
 
 const testMovRI = await Deno.readFile(".build/mov.ri.bin");
 
 // same as above, but different section of the asm file
 Deno.test({
-	// testing varying mov instructions, 32 bit
-	name: "test jasm - mov reg imm",
-	fn: async (t) => {
-		const { mov } = factory({
-			arch: "x86",
-			withFormat: "elf",
-		});
+  // testing varying mov instructions, 32 bit
+  name: "test jasm - mov reg imm",
+  fn: async (t) => {
+    const { mov } = factory({
+      arch: "x86",
+      withFormat: "elf",
+    });
 
-		const bytes = splitBytes(testMovRI, 5);
+    const bytes = splitBytes(testMovRI, 5);
 
-		await t.step({
-			name: "mov ebx, 1",
-			fn: () => {
-				const realValue = [...bytes.next().value];
-				assertEquals(mov("ebx", 1), realValue);
-			},
-		});
+    await t.step({
+      name: "mov ebx, 1",
+      fn: () => {
+        const realValue = [...bytes.next().value];
+        assertEquals(mov("ebx", 1), realValue);
+      },
+    });
 
-		await t.step({
-			name: "mov eax, -2",
-			fn: () => {
-				const realValue = [...bytes.next().value];
-				assertEquals(mov("eax", -2), realValue);
-			},
-		});
+    await t.step({
+      name: "mov eax, -2",
+      fn: () => {
+        const realValue = [...bytes.next().value];
+        assertEquals(mov("eax", -2), realValue);
+      },
+    });
 
-		await t.step({
-			name: "mov ecx, 200",
-			fn: () => {
-				const realValue = [...bytes.next().value];
-				assertEquals(mov("ecx", 200), realValue);
-			},
-		});
-	},
+    await t.step({
+      name: "mov ecx, 200",
+      fn: () => {
+        const realValue = [...bytes.next().value];
+        assertEquals(mov("ecx", 200), realValue);
+      },
+    });
+  },
 });
